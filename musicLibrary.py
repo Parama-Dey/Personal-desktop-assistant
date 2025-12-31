471647d485a9b62cd9e41c61de4f7e9278504a2d
Parama Dey <paramanova87@gmail.com>
2025-12-31 12:47:15 +0530

    File2
    This file contain some songs, for playing the songs on voice commands.

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music={
+    "alone":"https://www.youtube.com/watch?v=TrnEmXSuKk8",
+    "darkside":"https://www.youtube.com/watch?v=M-P4QBt-FWw",
+    "lily":"https://www.youtube.com/watch?v=ox4tmEV6-QU",
+    "faded":"https://www.youtube.com/watch?v=qdpXxGPqW-Y"
+    
+}
